0539417cdbf8a9fdfb84b5d6a41559a9be5f9dde
Ayushbahugunajii <ayushbahuguna76@gmail.com>
2024-11-04 00:48:24 +0530

    java script tutorial

---
+++ first.js
@@ -0,0 +1,54 @@
+//variable
+console.log('hello world');
+console.log('ayushbahuguna');
+age =18;
+console.log(age);
+price =18.9;
+console.log(price);
+x=null;
+console.log(x);
+y= undefined;
+console.log(y);
+bool=true;
+console.log(bool);
+
+//write form to write variable is camel case = "inAdd"
+
+
+//variale decleration in js
+var price=12.99;
+var price=12;
+console.log(price);// you can update var multipule time 
+
+ let newName="ashu";
+ newName="ayu";
+ console.log(newName); // you can not create a same variable but redfined it
+
+ const newAge= 19;// you can not update const variable or not change the type of variables 
+ console.log(newAge);
+
+
+ //data type
+
+ // 1. primitive DT
+
+ //number,string,bool,null,undefiend,null, bigInt ,Symbol
+
+// non-primitive
+ // array
+ //object
+const student={
+    studentName : "ayush",
+    // student Name = key and "ayush" = value
+    age:20,
+    rollNumber :6,
+};
+
+student ["age"]=student["age"]+1;
+
+console.log(student);
+
+console.log(typeof student);
+
+
+
